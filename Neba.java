bc1fafc8c182b9970c3854de3df0b7e349a6f173
nebiyu <83504562+tnebiyu@users.noreply.github.com>
2021-07-04 20:44:45 +0300

    Add files via upload
    it display the maximum and minimum of the given input

---
+++ Neba.java
@@ -0,0 +1,23 @@
+public class Neba {
+    public static void main(String[] args) {
+        int n = 28;
+        boolean b = isPerfect(n);
+        if (b)
+            System.out.println("it's perfect number");
+            else
+            System.out.println("it's not ");
+
+
+
+    }
+    public static boolean isPerfect(int n){
+        int sum = 0;
+        for (int i=1;i<n;i++){
+            if (n%i==0)
+                sum =sum + i;
+        }
+        if (n ==sum)
+            return true;
+        return false;
+    }
+}
